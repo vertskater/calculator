e488116cdf4b4bf54632f4851c118b7a8c4b242e
vertskater <vertskater@gmx.at>
2020-09-16 16:42:01 +0200

    added Math operations

--- script.js
+++ script.js
@@ -3,51 +3,76 @@
 class Calculator {
     constructor() {
         this.display = document.getElementById('display');
-        this.digits = document.querySelectorAll('.grid');
+        this.digits = document.querySelectorAll('.number');
+        this.operators = document.querySelectorAll('.operator');
         this.displayValues = [];
         this.displayValue = "";
+        this.operatorMath = "";
+        this.result = 0;
         this.populateDisplay();
+        this.compute();
     }
 
-    add(x, y) {
-        return x + y;
-    }
-    substract(x, y) {
-        return x - y;
-    }
-    multiply(x, y) {
-        return x * y;
-    }
-    divide(x, y) {
-        return x / y;
-    }
-    operate(array) {
-        if (array[1] == '+') {
-            return this.add(x, y);
-        } else if (array[1] == '-') {
-            return this.substract(x, y);
-        } else if (array[1] == '*') {
-            return this.multiply(x, y)
+    operate(values, operator) {
+        if (operator == '+') {
+            this.result = parseInt(values[0]) + parseInt(values[1]);
+            this.display.textContent = this.result + this.operatorMath;
+            this.displayValues = [];
+            this.displayValue = "";
+            this.displayValues.push(this.result);
+            console.log(this.displayValues);
+        } else if (operator == '-') {
+            this.result = parseInt(values[0]) - parseInt(values[1]);
+            this.display.textContent = this.result + this.operatorMath;;
+            this.displayValues = [];
+            this.displayValue = "";
+            this.displayValues.push(this.result);
+        } else if (operator == '*') {
+            this.result = parseInt(values[0]) * parseInt(values[1]);
+            this.display.textContent = this.result + this.operatorMath;;
+            this.displayValues = [];
+            this.displayValue = "";
+            this.displayValues.push(this.result);
         } else {
-            return this.divide(x, y);
+            this.result = parseInt(values[0]) / parseInt(values[1]);
+            this.display.textContent = this.result + this.operatorMath;;
+            this.displayValues = [];
+            this.displayValue = "";
+            this.displayValues.push(this.result);
         }
+    }
+    compute(){
+        this.operators.forEach(operator =>{
+            operator.addEventListener('click', ()=>{
+                this.operatorMath = operator.getAttribute('data-value');
+                console.log(this.displayValues.length);
+                if(this.displayValues.length == 0){
+                    this.displayValues.push(this.displayValue);
+                    this.result = this.displayValue;
+                    this.displayValue = '';
+                    this.display.textContent = this.result + this.operatorMath;
+                    console.log(this.operatorMath);
+                }else if(this.displayValues.length == 1){
+                    this.displayValues.push(this.displayValue);
+                    this.operate(this.displayValues, this.operatorMath);
+                    // console.log(this.operatorMath);
+                }else if(this.displayValues.length == 2){
+                    this.displayValues.push(this.displayValue);
+                    this.operate(this.displayValues, this.operatorMath);
+                }
+            })
+        })
     }
     populateDisplay() {
         this.digits.forEach(digit => {
             digit.addEventListener('click', () => {
-                if ((digit.getAttribute('data-value') >= '0') && (digit.getAttribute('data-value') <= 9)) {
                     this.displayValue += digit.getAttribute('data-value');//.push(digit.getAttribute('data-value'));
                     this.display.textContent += digit.getAttribute('data-value');
                     console.log(this.displayValue);
-                } else if ((digit.getAttribute('data-value') == '+') || (digit.getAttribute('data-value') == '-') || (digit.getAttribute('data-value') == '*') || (digit.getAttribute('data-value') == '/')) {
-                    this.displayValues.push(this.displayValue);
-                    this.displayValues.push(digit.getAttribute('data-value'));
-                    this.displayValue = '';
-                    this.display.textContent += digit.getAttribute('data-value');
-                    console.log(this.displayValues);
-                } else if (digit.getAttribute('data-value') == 'clear'){
-                    this.clearAll();
-                }
+
+                    if(digit.getAttribute('data-value') == 'clear'){
+                        this.clearAll();
+                    }
             });
         })
     }
